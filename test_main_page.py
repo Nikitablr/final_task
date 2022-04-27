d14050dbf837602ffde3dc5f7c2f65f936a343af
nikrud <89258843+nikrud@users.noreply.github.com>
2022-04-27 17:41:53 +0300

    added updates

--- test_main_page.py
+++ test_main_page.py
@@ -1,13 +1,22 @@
-import time
-from selenium.webdriver.common.by import By
+import pytest
 
-link = "http://selenium1py.pythonanywhere.com/"
+from pages.login_page import LoginPage
+from .pages.main_page import MainPage
 
-class MainPage():
-    def test_guest_can_go_to_login_page(browser):
-        browser.get(link)
-        go_to_login_page(browser)
 
-    def go_to_login_page(browser):
-        login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+
+    def test_guest_can_go_to_login_page(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com/"
+        self.page = MainPage(browser, self.link)
+        self.page.open()
+        self.page.go_to_login_page()
+        self.login_page = LoginPage(browser, browser.current_url)
+        self.login_page.should_be_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        self.link = "http://selenium1py.pythonanywhere.com/"
+        self.page = MainPage(browser, self.link)
+        self.page.open()
+        self.page.should_be_login_link()
